9f1ead21784f1dacaa6653990526924e80e3d270
vAHiD2535 <136799059+vAHiD55555@users.noreply.github.com>
2025-08-08 03:11:53 +0330

    Add files via upload

---
+++ proxy_sources.py
@@ -0,0 +1,17 @@
+# proxy_sources.py
+
+# List of raw proxy source URLs
+PROXY_SOURCE_URLS = [
+    "https://raw.githubusercontent.com/vAHiD55555/getproxy/refs/heads/master/file/all.txt",
+    # Future sources can be added here
+]
+
+# Optionally, categorize by protocol if needed
+PROXY_SOURCE_CATEGORIES = {
+    "http": [
+        "https://raw.githubusercontent.com/vAHiD55555/getproxy/refs/heads/master/file/https.txt"
+    ],
+    "socks5": [
+        "https://raw.githubusercontent.com/vAHiD55555/getproxy/refs/heads/master/file/socks5.txt"
+    ]
+}
